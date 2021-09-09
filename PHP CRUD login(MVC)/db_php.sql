e264a628523fee4bde90c0ef987afb4dfd1040ff
marianoq <quirogamariano@gmail.com>
2021-09-09 10:56:44 -0300

    bd

---
+++ PHP CRUD login(MVC)/db_php.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-09-2021 a las 15:53:45
+-- Versión del servidor: 10.4.20-MariaDB
+-- Versión de PHP: 7.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_php`
+--
+CREATE DATABASE IF NOT EXISTS `db_php` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `db_php`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registros`
+--
+
+CREATE TABLE `registros` (
+  `id` int(11) NOT NULL,
+  `token` text NOT NULL,
+  `nombre` text NOT NULL,
+  `email` text NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `intentos_fallidos` int(11) DEFAULT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `registros`
+--
+ALTER TABLE `registros`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `registros`
+--
+ALTER TABLE `registros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- PHP CRUD login(MVC)/modelos/conexion.php
+++ PHP CRUD login(MVC)/modelos/conexion.php
@@ -2,7 +2,7 @@
 
 class Conexion{
 	static public function conectar(){
-		$link = new PDO("mysql:host=localhost;dbname=curso-php",
+		$link = new PDO("mysql:host=localhost;dbname=db_php",
 						"root",
 						"");
 		$link->exec("set name utf8");
